72df0b46f3afb05d128e2f8d8153324b9cf747d0
LucasMorgani <morganis1411@gmail.com>
2025-07-15 15:45:16 -0300

    feat: remote backend

--- provider.tf
+++ provider.tf
@@ -1,9 +1,19 @@
+# Definindo qual será o provider
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "6.3.0"
     }
+  }
+}
+
+# Passando o tfstate para um bucket S3
+terraform {
+  backend "s3" {
+    bucket = "skylab-comunidadedevops"
+    key    = "dev/terraform.tfstate"
+    region = "us-east-1"
   }
 }
 
