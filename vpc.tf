436d62f09d1fbb8320110b13255adf9bd56cab9f
LucasMorgani <morganis1411@gmail.com>
2025-07-15 15:45:53 -0300

    fix: lint

---
+++ vpc.tf
@@ -0,0 +1,9 @@
+# Setando a VPC
+resource "aws_vpc" "eks_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "skylab-vpc"
+  }
+}
